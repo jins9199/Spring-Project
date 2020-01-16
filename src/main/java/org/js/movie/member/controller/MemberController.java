942b6bab66e55fadd00b6db45aef17ff25e69887
jins9199 <jins9199@daum.net>
2020-01-16 13:55:49 +0900

    afaf

--- src/main/java/org/js/movie/member/controller/MemberController.java
+++ src/main/java/org/js/movie/member/controller/MemberController.java
@@ -2,8 +2,7 @@ package org.js.movie.member.controller;
 
 import org.js.movie.member.domain.MemberVO;
 import org.js.movie.member.service.MemberService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
