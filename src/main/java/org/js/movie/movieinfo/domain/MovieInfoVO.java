24c594d014a8000e30c5a6d64749083f03c9e73e
lazy1 <lazy1@LAPTOP-3FJ9A128>
2020-01-22 17:25:43 +0900

    0122a

--- src/main/java/org/js/movie/movieinfo/domain/MovieInfoVO.java
+++ src/main/java/org/js/movie/movieinfo/domain/MovieInfoVO.java
@@ -6,9 +6,37 @@ import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 public class MovieInfoVO {	
+	public int getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
 	private int movieId;
 	private String title;
 	private Date regDate;
